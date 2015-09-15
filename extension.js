db733a4d9511b222ee5164459bf3b1ad79358321
RyanballTagPro <ryanballwasalreadytakenwtf@gmail.com>
2015-09-15 18:40:01 +1000

    Update extension.js

--- extension.js
+++ extension.js
@@ -100,7 +100,7 @@
       fbLink: null,
       youtubeLink: "https://www.youtube.com/user/oceanicTagPro",
       website: "https://www.reddit.com/r/oceanictagpro",
-      intervalMessages: ["#GiveiTagMinutes","Welcome to the Oceanic Tagpro plug dj!", "Play Now! http://tagpro-diameter.koalabeast.com"],
+      intervalMessages: ["#GiveiTagMinutes","Welcome to the Oceanic Tagpro plugdj! Find us at /r/OceanicTagpro", "Play Now! http://tagpro-diameter.koalabeast.com", "!motd"],
       messageInterval: 1,
       songstats: false,
       commandLiteral: "!",
